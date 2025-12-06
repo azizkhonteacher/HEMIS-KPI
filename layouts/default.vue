6cbb1eca34835b41e276c3ada61671be02078c80
azizkhonteacher <azizkhonteacher@gmail.com>
2025-12-06 16:53:14 +0500

    First Commit

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <ul class="categories">
+        <li><nuxt-link to="/">Dashboard</nuxt-link></li>
+        <li><nuxt-link to="/">Requests</nuxt-link></li>
+        <li><nuxt-link to="/">Payroll</nuxt-link></li>
+        <li><nuxt-link to="/">Company</nuxt-link></li>
+        <li><nuxt-link to="/">Extras</nuxt-link></li>
+      </ul>
+    </header>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
